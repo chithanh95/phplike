608ee22f1a38e004044320002a90c59e5e51348c
chithanh95 <mr.chithanh1@gmail.com>
2016-08-12 23:57:20 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+#Xem hướng dẫn tại : https://www.youtube.com/watch?v=VQQX6kAELb4
+$dataLog  =  array(
+'email' => 'curllike113@gmail.com',
+'pass' => 'loncac123',
+'apps' =>'41158896424',# ID Applikasi nya
+);
+
+?>
